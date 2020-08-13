a0dbca875aba9d478f5f862cab46545572d54bd9
fuguilin <fuguilin@axzo.cn>
2020-08-13 15:34:03 +0800

    add:新增抽象类的继承和实现

--- 02.abstract-class.dart
+++ 02.abstract-class.dart
@@ -1,10 +1,30 @@
 /****
  * 抽象类
- * 使用abstract
- * 
+ * 使用abstract 关键字去定义一个抽象类
+ *  1.定义方法不实现，让子类去实现
+ *  2.不可直接实现抽象类--调用时体现
  * 
  */
 
-abstract class Http {}
+abstract class Http {
+  get();
+  post();
+}
 
-main(List<String> args) {}
+class Request extends Http {
+  @override
+  get() {
+    print('get request');
+  }
+
+  @override
+  post() {
+    print('post request');
+  }
+}
+
+main(List<String> args) {
+  Request request = new Request();
+  request.get();
+  request.post();
+}
